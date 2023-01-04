43e32d696a54824e4808bfbfae7475ca4853cbba
mihajlovskakate <mihajlovskakate1@gmail.com>
2023-01-04 09:51:36 +0100

    Change router

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./components/About"
 import Footer from "./components/Footer"
 import Contact from "./components/Contact/Contact"
 import ThankYou from "./components/Contact/ThankYou"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 
 
@@ -14,18 +14,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
 
    return (
-    <Router>
+   <>
     <Navbar/>
     <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/projects" element={<Projects />} />  
       <Route path="/contact" element={<Contact />} />   
       <Route path="/thankyou" element={<ThankYou />} /> 
     </Routes>
     <Footer />
-  </Router>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+<React.StrictMode>
+<HashRouter >
+<App />
+</HashRouter>
+</React.StrictMode>
+);
 
